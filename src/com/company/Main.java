de0e6e8253c2637d409f2a35bb444c223b494ee6
liujinjie <liujinjie170818@credithc.com>
2019-10-08 18:42:22 +0800

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,3 +10,4 @@ public class Main {
 }
 // java  project has  src  out  External Libraries  src com  company  out production  hello_demo  .idea project 配置文件目录, .iml 为Module 配置文件。
 // this is  javascript  common src out, think someting sleep just test
+// liujinjie
